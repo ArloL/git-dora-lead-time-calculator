0aae007ebe1f46384a22d861f247c921e207a04a
Arlo O'Keeffe <tiger@k5d.de>
2026-06-22 12:28:43 +0200

    use integration test

---
+++ src/test/java/io/github/arlol/dora/GitDoraLeadTimeCalculatorTests.java
@@ -0,0 +1,14 @@
+package io.github.arlol.dora;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class GitDoraLeadTimeCalculatorTests {
+
+	@Test
+	public void isTrue() throws Exception {
+		assertTrue(true);
+	}
+
+}
