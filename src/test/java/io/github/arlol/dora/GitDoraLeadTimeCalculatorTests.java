b9dabee78c1fd7c7ca8df7161ebe3aaac654c061
Arlo O'Keeffe <tiger@k5d.de>
2026-06-29 21:46:24 +0200

    write a basic unit test

--- src/test/java/io/github/arlol/dora/GitDoraLeadTimeCalculatorTests.java
+++ src/test/java/io/github/arlol/dora/GitDoraLeadTimeCalculatorTests.java
@@ -2,13 +2,34 @@ package io.github.arlol.dora;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
-public class GitDoraLeadTimeCalculatorTests {
+class GitDoraLeadTimeCalculatorTests {
 
 	@Test
-	public void isTrue() throws Exception {
-		assertTrue(true);
+	void versionPrintsTitleAndVersion() {
+		String out = captureOut(
+				() -> GitDoraLeadTimeCalculatorApplication.main("--version")
+		);
+		assertTrue(
+				out.contains("version \""),
+				"Expected --version output to contain 'version \"'"
+		);
+	}
+
+	private static String captureOut(Runnable runnable) {
+		PrintStream previous = System.out;
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(buffer));
+		try {
+			runnable.run();
+		} finally {
+			System.setOut(previous);
+		}
+		return buffer.toString();
 	}
 
 }
